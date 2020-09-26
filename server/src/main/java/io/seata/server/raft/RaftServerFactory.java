d5b063ece46eb6369ba4594a2ab2c2419317d4c1
FUNKYE <364176773@qq.com>
2020-09-26 15:49:15 +0800

    optimize code

--- server/src/main/java/io/seata/server/raft/RaftServerFactory.java
+++ server/src/main/java/io/seata/server/raft/RaftServerFactory.java
@@ -1,3 +1,18 @@
+/*
+ *  Copyright 1999-2019 Seata.io Group.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package io.seata.server.raft;
 
 import java.io.IOException;
@@ -13,6 +28,9 @@ import org.slf4j.LoggerFactory;
 
 import static io.seata.common.DefaultValues.SEATA_RAFT_GROUP;
 
+/**
+ * @author funkye
+ */
 public class RaftServerFactory {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(RaftServerFactory.class);
