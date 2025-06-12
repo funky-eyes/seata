8d153308cd423ea2c3dcb56af77a3a3f16e90319
funky-eyes <jianbin@apache.org>
2025-06-12 23:08:07 +0800

    test

--- namingserver/src/main/java/org/apache/seata/namingserver/manager/NamingManager.java
+++ namingserver/src/main/java/org/apache/seata/namingserver/manager/NamingManager.java
@@ -246,6 +246,9 @@ public class NamingManager {
     }
 
     public boolean registerInstance(NamingServerNode node, String namespace, String clusterName, String unitName) {
+        if (node == null) {
+            return false;
+        }
         try {
             Map<String, ClusterData> clusterDataHashMap =
                 namespaceClusterDataMap.computeIfAbsent(namespace, k -> new ConcurrentHashMap<>());
@@ -326,6 +329,9 @@ public class NamingManager {
 
     public List<Node> getInstances(String namespace, String clusterName, boolean readOnly) {
         Map<String, ClusterData> clusterDataHashMap = namespaceClusterDataMap.get(namespace);
+        if (clusterDataHashMap == null) {
+            return Collections.emptyList();
+        }
         ClusterData clusterData = clusterDataHashMap.get(clusterName);
         if (clusterData == null) {
             LOGGER.warn("no instances in {} : {}", namespace, clusterName);
@@ -335,6 +341,7 @@ public class NamingManager {
             : clusterData.getInstanceList().stream()
                 .filter(node -> node.getRole() == ClusterRole.LEADER || node.getRole() == ClusterRole.MEMBER)
                 .collect(Collectors.toList());
+
     }
 
     public List<Node> getInstancesByVgroupAndNamespace(String namespace, String vgroup, boolean readOnly) {
