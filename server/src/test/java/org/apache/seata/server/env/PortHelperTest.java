b04be25f28ddae7389a07ed51ae2591f60f10d9d
jianbin <jianbin@apache.org>
2025-06-13 09:45:26 +0800

    try fix the ut

--- server/src/test/java/org/apache/seata/server/env/PortHelperTest.java
+++ server/src/test/java/org/apache/seata/server/env/PortHelperTest.java
@@ -32,7 +32,7 @@ class PortHelperTest {
 
     @Test
     public void testGetPortFromConfigFile() throws IOException {
-        Assertions.assertEquals(8091,PortHelper.getPortFromConfigFile());
+        Assertions.assertEquals(8080, PortHelper.getPortFromConfigFile());
     }
 
 }
